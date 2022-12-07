c8767c88c207d45c53f7453cc0a507dfb25c4d75
Anna Nickelson <aanickelson@gmail.com>
2022-12-06 16:34:13 -0800

    Finalized state and actions. Made minor cleanup. Need to finish cleanup and debugging.
    Have not completely implemented G / rewards through the POIs. Need to check that they are being marked as observed / make sure they can be captured.

--- Agent.py
+++ Agent.py
@@ -10,30 +10,32 @@ class Agent:
         self.p = p
         self.curr_rm = None
         self.type = type            # Agent type - TODO: this should not be hard-coded
-        self.goal = None             # variable to store desired goal
+        self.xy_goal = None             # variable to store desired goal
+        self.poi = None
         # self.capabilities = cap     # randomly initialize agent's capability of viewing each POI
         self.capabilities = np.ones_like(cap)       # Agents are equally good at finding all POIs
         self.policy = None
         self.state = None           # Current state
-        self.rm_timers = np.zeros(len(self.p.rooms) + 1) + 100   # Keep track of how long it has been since last in each room - everything starts as 'never been'
+        self.rm_timers = np.zeros(len(self.p.rooms)) + 100   # Keep track of how long it has been since last in each room - everything starts as 'never been'
         self.rm_in_state = np.zeros_like(self.rm_timers)    # Binary to determine whether info drops out of state (0 don't include / 1 include)
 
     def reset(self):
-        self.goal = None             # reset to no desired POI
+        self.xy_goal = None             # reset to no desired POI
+        self.poi = None
+        self.curr_rm = None
         self.policy = None
         self.state = None
 
     def step(self):
-        if self.goal:
+        if self.xy_goal:
             self.move()                     # move agent toward POI
             rew = self.observe()
             if rew:                         # If at the POI and observed
-                poi = self.goal              # get the POI
+                poi = self.poi              # get the POI
                 poi.viewing.append(self)    # add the agent to current agents viewing the POI
                 poi.viewed.append(self)
-                poi.viewed_rew.append(rew)
-                self.goal.claimed = False
-                self.goal = None
+                # poi.viewed_rew.append(rew)
+                self.xy_goal = None
 
     # moves agent 1-unit towards the POI
     def move(self):
@@ -41,9 +43,9 @@ class Agent:
         If the agent has a desired POI, move one unit toward POI
         :return:
         """
-        if self.goal is not None:
-            X = self.goal[0]
-            Y = self.goal[1]
+        if self.xy_goal is not None:
+            X = self.xy_goal[0]
+            Y = self.xy_goal[1]
             R = ((X-self.x)**2.0+(Y-self.y)**2.0)**0.5
             if R > 1:
                 self.y += (Y-self.y)/R
@@ -58,11 +60,11 @@ class Agent:
         If agent is within the observation radius, it is successful in observing
         :return:
         """
-
-        if self.goal.class_type == 'Agent':
+        # If there is no POI,
+        if not self.poi:
             return 0
-        if abs(self.goal.x - self.x) <= self.goal.obs_radius and abs(self.goal.y - self.y) <= self.goal.obs_radius:
-            return self.goal.curr_rew
+        elif abs(self.poi.x - self.x) <= self.poi.obs_radius and abs(self.poi.y - self.y) <= self.poi.obs_radius:
+            return self.poi.value
         else:
             return 0
 
--- room.py
+++ room.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import sqrt
 
 class Room:
     def __init__(self, idx, p, bounds, door, pois):
@@ -6,7 +7,13 @@ class Room:
         self.p = p
         self.bounds = bounds  # [lower left, upper right] - [[x1, y1], [x2, y2]]
         self.pois = pois
-        self.door = door
+        self.door = None
+        if door:
+            self.door = [door[0], door[1]]   # Door comes in as a tuple instead of a list so this is a dumb easy fix
+
+        self.agents_in_rm = []
+
+    def reset(self):
         self.agents_in_rm = []
 
     def in_room(self, agent):
@@ -20,6 +27,11 @@ class Room:
 
         if lower[0] < agent.x < upper[0] and lower[1] < agent.y < upper[1]:
             return True
+        # If the agent is in the door, it's in the room.
+        if self.door:
+            dist_to_door = sqrt((agent.x - self.door[0]) ** 2 + (agent.y - self.door[1]) ** 2)
+            if dist_to_door < 0.05:
+                return True
         return False
 
     def poi_state(self):
@@ -33,7 +45,7 @@ class Room:
         # Theoretically this part could be calculated only once with how it is set up right now
         # BUT I'm leaving it this way because we will likely have POIs that appear / disappear at some point
         for poi in self.pois:
-            st[poi.poi_type] += 1
+            st[poi.type] += 1
         ag_st = [0] * self.p.n_agent_types
         # How many of each agent type are in this room
         for ag in self.agents_in_rm:
